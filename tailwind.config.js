69a561186380d00f35f31659bc3b30c6251d20e2
kynhix <kristopher.t.hicks@gmail.com>
2023-12-01 01:15:26 -0500

    Completely revamped the design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-move-in': 'fade-move-in 0.75s ease-out forwards',
+      },
+      dropShadow: {
+        'glow': '0 0 100px rgba(99, 114, 255, 0.13)',
+      },
+      boxShadow: {
+        'button-glow': '0px 6px 11.7px 0px rgba(102, 102, 102, 0.25) inset'
+      }
+    },
   },
   plugins: [],
 }
